22f2bed4f6f09f369825c90e0be1fdd1f64b299f
Szczepan Masny <szczepanmasny@gmail.com>
2020-11-27 11:47:14 +0100

    reorganize files structure

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,31 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  roots: ['<rootDir>/src/library'],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/?(*.)+(spec).+(ts|tsx|js)'],
   transform: {
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$': 'jest-transform-stub'
+  },
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'json',
+    'vue'
+  ],
+  moduleNameMapper: {
+    '^~/(.*)$': '<rootDir>/$1',
+    '^~~/(.*)$': '<rootDir>/$1'
   },
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts']
+  verbose: true,
+  coveragePathIgnorePatterns: ['/node_modules/', '/.node/'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { App } from 'vue';
 import { BemOptions } from '@/models';
-import { install } from './plugin';
+import { install } from './library';
 
 export function createBem (options: BemOptions): {install: (T: App) => void} {
   return {
